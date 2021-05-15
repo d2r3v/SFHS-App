a484a62e60f8dc6872bf82dde1e1db8281b77fb2
d2r3v <hereisdhruv007@gmail.com>
2021-05-15 23:24:01 +0530

    Updated Css

--- src/Components/Login/login.jsx
+++ src/Components/Login/login.jsx
@@ -19,8 +19,8 @@ export class Login extends React.Component{
                 </div>
                 <div className="form">
                     <div className="form-group">
-                        <label htmlFor="Username">UserName </label>
-                        <input type="text" name="username" placeholder="UserName" />
+                        <label htmlFor="Username">Username </label>
+                        <input type="text" name="username" placeholder="Username" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password </label>
