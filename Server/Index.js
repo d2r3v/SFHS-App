754524791cd10e41eca429f6b286ab275f4590e0
d2r3v <hereisdhruv007@gmail.com>
2021-05-20 00:02:40 +0530

    Created Server/Index.js

---
+++ Server/Index.js
@@ -0,0 +1,22 @@
+// server/index.js
+
+const express = require("express");
+const mysql =require("mysql");
+
+const app = express();
+
+app.use(express.json());
+
+const db=mysql.createConnection(
+    {
+        user:"root",
+        host:"localhost",
+        password:"password",
+        database:"LoginSystem",
+
+    }
+);
+
+app.listen(3001, ()=>{
+    console.log("Running Server");
+});
